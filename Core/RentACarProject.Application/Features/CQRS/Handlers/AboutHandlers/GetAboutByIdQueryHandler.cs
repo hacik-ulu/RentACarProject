3d83f2ffe18ce16eb63e7df362ddabf0457fbbd1
Haçik Ulu <hacikd2@gmail.com>
2024-02-21 23:12:03 +0300

    GetAboutQueryHandler edited.

---
+++ Core/RentACarProject.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using RentACarProject.Application.Features.CQRS.Queries.AboutQueries;
+using RentACarProject.Application.Features.CQRS.Results.AboutResults;
+using RentACarProject.Application.Interfaces;
+using RentACarProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Application.Features.CQRS.Handlers.AboutHandlers;
+
+public class GetAboutByIdQueryHandler
+{
+    private readonly IRepository<About> _repository;
+
+    public GetAboutByIdQueryHandler(IRepository<About> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+    {
+        // values'dan sonuç olarak gelecek değerleri GetAboutByIdQueryResult tipine çevirmeliyiz.
+        var values = await _repository.GetByIdAsync(query.Id);
+        return new GetAboutByIdQueryResult
+        {
+            AboutId = values.AboutId,
+            Description = values.Description,
+            ImageUrl = values.ImageUrl,
+            Title = values.Title,
+        };
+    }
+
+}
---
+++ Core/RentACarProject.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using RentACarProject.Application.Features.CQRS.Results.AboutResults;
+using RentACarProject.Application.Interfaces;
+using RentACarProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Application.Features.CQRS.Handlers.AboutHandlers;
+
+public class GetAboutQueryHandler
+{
+    private readonly IRepository<About> _repository;
+
+    public GetAboutQueryHandler(IRepository<About> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<GetAboutQueryResult>> Handle()
+    {
+        //Seçilen nesnelerin(About) tüm özellikleri tek tek GetAboutQueryResult tipine dönüştürülerek,
+        //yeni bir listeden seçim yapıyoruz.
+
+        var values = await _repository.GetAllAsync();
+        return values.Select(x => new GetAboutQueryResult
+        {
+            AboutId = x.AboutId,
+            Description = x.Description,
+            Title = x.Title,
+            ImageUrl = x.ImageUrl
+        }).ToList();
+    }
+}
