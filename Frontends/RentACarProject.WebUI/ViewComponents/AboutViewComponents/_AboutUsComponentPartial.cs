09f010ae85edff344e811dc2dca766f032b22545
Haçik Ulu <hacikd2@gmail.com>
2024-03-08 19:29:41 +0300

    About editing.

--- Frontends/RentACarProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/RentACarProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.AboutDtos;
 
 namespace RentACarProject.WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
 
         }
