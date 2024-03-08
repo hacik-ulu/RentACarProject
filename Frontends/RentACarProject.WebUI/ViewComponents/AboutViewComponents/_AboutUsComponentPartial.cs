06248c5f333a94978e3f23c4041c87c0ee6c3be3
Haçik Ulu <hacikd2@gmail.com>
2024-03-08 19:07:45 +0300

    _AboutUsComponentPartial Added.

---
+++ Frontends/RentACarProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+    }
+}
