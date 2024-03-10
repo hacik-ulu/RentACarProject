2199e382fafaed5733a0d69923647c00c3efa037
Haçik Ulu <hacikd2@gmail.com>
2024-03-10 14:15:04 +0300

    Contact (FooterAddress) edited.

---
+++ Frontends/RentACarProject.WebUI/ViewComponents/FooterAddressComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.AboutDtos;
+using RentACarProject.Dto.FooterAddressDtos;
+
+namespace RentACarProject.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+    }
+}
