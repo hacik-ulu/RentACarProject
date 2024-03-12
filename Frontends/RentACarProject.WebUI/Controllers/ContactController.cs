69fe0032df82ea81c9fa2d9240fecf6f45eb9b22
Haçik Ulu <hacikd2@gmail.com>
2024-03-12 19:59:42 +0300

    Contact footeraddress and message finished.

--- Frontends/RentACarProject.WebUI/Controllers/ContactController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.ContactDtos;
+using RentACarProject.WebUI.Models;
+using System.Text;
 
 namespace RentACarProject.WebUI.Controllers
 {
@@ -11,9 +15,33 @@ namespace RentACarProject.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7262/api/Contacts", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Your message has been sent successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+
+
     }
 }
+
