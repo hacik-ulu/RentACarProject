59461bf19f1a5e936df78a765211f317288bd86d
Haçik Ulu <hacikd2@gmail.com>
2024-03-13 18:24:50 +0300

    Default (Home Page) started to editing.

---
+++ Frontends/RentACarProject.WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.BannerDtos;
+
+namespace RentACarProject.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCoverUILayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+    }
+}
