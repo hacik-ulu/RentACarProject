099765480b124e83bc2a56e5a69f51b9914fb095
Haçik Ulu <hacikd2@gmail.com>
2024-03-16 21:03:30 +0300

    Default Page is editing.

---
+++ Frontends/RentACarProject.WebUI/ViewComponents/DefaultViewComponents/_DefaultLastFiveCarsWithBrandsComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.CarDtos;
+using RentACarProject.Dto.TestimonialDtos;
+
+namespace RentACarProject.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultLastFiveCarsWithBrandsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLastFiveCarsWithBrandsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Cars/GetLastFiveCarsWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastFiveCarsWithBrandsDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
