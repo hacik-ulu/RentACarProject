31ad435c022d1a400414b45b59fa2543b525b915
Haçik Ulu <hacikd2@gmail.com>
2024-03-20 22:14:48 +0300

    Blog and Author entities added to project.

--- Infrastructure/RentACarProject.Persistence/Context/RentACarContext.cs
+++ Infrastructure/RentACarProject.Persistence/Context/RentACarContext.cs
@@ -25,5 +25,7 @@ public class RentACarContext : DbContext
     public DbSet<Service> Services { get; set; }
     public DbSet<SocialMedia> SocialMedias { get; set; }
     public DbSet<Testimonial> Testimonials { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
 
 }
