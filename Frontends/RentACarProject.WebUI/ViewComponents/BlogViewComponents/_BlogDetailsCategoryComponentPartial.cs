94ca5e848097dfbf945ecb2f280fdf085c52f003
Haçik Ulu <hacikd2@gmail.com>
2024-03-27 13:28:03 +0300

    BlogDetailsCategoryComponentPartial edited.

--- Frontends/RentACarProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryComponentPartial.cs
+++ Frontends/RentACarProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryComponentPartial.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.AboutDtos;
+using RentACarProject.Dto.CategoryDtos;
 
 namespace RentACarProject.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsCategoryComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsCategoryComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
+
         }
     }
 }
