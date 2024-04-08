f4c242dabd93be157f7afe524897f76b39afb0bb
Haçik Ulu <hacikd2@gmail.com>
2024-04-08 23:47:41 +0300

    Admin Navbar,Sidebar and Footer edited.

---
+++ Frontends/RentACarProject.WebUI/ViewComponents/AdminViewComponents/_AdminSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.ViewComponents.AdminViewComponents
+{
+    public class _AdminSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
