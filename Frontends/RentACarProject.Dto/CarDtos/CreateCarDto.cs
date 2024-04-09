aa14aab0ab9baabc68f7bf9735a816b3209118db
Haçik Ulu <hacikd2@gmail.com>
2024-04-09 23:28:59 +0300

    CreateCarController is editing.

---
+++ Frontends/RentACarProject.Dto/CarDtos/CreateCarDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Dto.CarDtos
+{
+    public class CreateCarDto
+    {
+        public int BrandID { get; set; }
+        public string Model { get; set; }
+        public string CoverImagerUrl { get; set; }
+        public decimal Mileage { get; set; }
+        public int Year { get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+    }
+}
--- Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RentACarProject.Dto.BrandDtos;
 using RentACarProject.Dto.CarDtos;
 
 namespace RentACarProject.WebUI.Controllers
@@ -24,6 +26,26 @@ namespace RentACarProject.WebUI.Controllers
                 return View(values);
             }
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.BrandID.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+            return View();
+
+            // Fuel için ayrı bir entity oluşturup daha sonrasında sadece listeleme işlemi MEDİATOR ile yapılıp API call edilebilir sonrasında dropdown olabilir
+
         }
     }
 }
