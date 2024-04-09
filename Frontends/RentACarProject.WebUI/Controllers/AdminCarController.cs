b36e449bd946df9e74be1702062dfc52249a5976
Haçik Ulu <hacikd2@gmail.com>
2024-04-09 21:15:38 +0300

    CarList is editing.

--- Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.CarDtos;
 
 namespace RentACarProject.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
