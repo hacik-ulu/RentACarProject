7a9611bced1f322a1af88bb84d3e88c0279b8e2e
Haçik Ulu <hacikd2@gmail.com>
2024-04-10 17:02:15 +0300

    Fuel Type edited.

--- Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/AdminCarController.cs
@@ -43,18 +43,28 @@ namespace RentACarProject.WebUI.Controllers
                                                     Value = x.BrandID.ToString()
                                                 }).ToList();
 
-            // For Transmission Type Dropdpwn List
+            // Transmission Type Dropdown List
             List<SelectListItem> transmissionValues = new List<SelectListItem>
-    {
-        new SelectListItem { Text = "Manual Transmission", Value = "Manual" },
-        new SelectListItem { Text = "Automatic Transmission", Value = "Automatic" },
-        new SelectListItem { Text = "Semi-Automatic Transmission", Value = "Semi-Automatic" },
-        new SelectListItem { Text = "Direct Drive (Fixed Gear)", Value = "Direct Drive" },
-        new SelectListItem { Text = "Dual-Clutch Transmission (DSG or PDK)", Value = "Dual-Clutch" }
-    };
+            {
+                new SelectListItem { Text = "Manual Transmission", Value = "Manual" },
+                new SelectListItem { Text = "Automatic Transmission", Value = "Automatic" },
+                new SelectListItem { Text = "Semi-Automatic Transmission", Value = "Semi-Automatic" },
+                new SelectListItem { Text = "Direct Drive (Fixed Gear)", Value = "Direct Drive" },
+                new SelectListItem { Text = "Dual-Clutch Transmission (DSG or PDK)", Value = "Dual-Clutch" }
+            };
+
+
+            // Fuel Type Dropdown List
+            List<SelectListItem> fuelTypeValues = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Gasoline", Value = "Gasoline" },
+                new SelectListItem { Text = "Diesel", Value = "Diesel" },
+                new SelectListItem { Text = "Electricity", Value = "Electricity" }
+            };
 
             ViewBag.BrandValues = brandValues;
             ViewBag.TransmissionValues = transmissionValues;
+            ViewBag.FuelTypeValues = fuelTypeValues;
 
             return View();
 
