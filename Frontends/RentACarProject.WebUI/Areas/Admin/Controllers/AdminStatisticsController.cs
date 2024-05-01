03390ed616ff823a581a78a4ec938a0a09d14cec
Haçik Ulu <hacikd2@gmail.com>
2024-05-01 23:17:40 +0300

    AdminStatistics editing.

---
+++ Frontends/RentACarProject.WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminStatistics")]
+    public class AdminStatisticsController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
