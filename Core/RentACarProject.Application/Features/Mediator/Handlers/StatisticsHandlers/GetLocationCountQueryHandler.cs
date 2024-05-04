1f5fc1ced69287b9db6d405eca3ed756b4885b19
Haçik Ulu <hacikd2@gmail.com>
2024-05-04 23:01:19 +0300

    Handlers are editing.

---
+++ Core/RentACarProject.Application/Features/Mediator/Handlers/StatisticsHandlers/GetLocationCountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using RentACarProject.Application.Features.Mediator.Queries.StatisticsQueries;
+using RentACarProject.Application.Features.Mediator.Results.StatisticsResult;
+using RentACarProject.Application.Interfaces.CarInterfaces;
+using RentACarProject.Application.Interfaces.StatisticsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetLocationCountQueryHandler : IRequestHandler<GetLocationCountQuery, GetLocationCountQueryResult>
+    {
+        private readonly IStatisticsRepository _repository;
+
+        public GetLocationCountQueryHandler(IStatisticsRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetLocationCountQueryResult> Handle(GetLocationCountQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetLocationCountAsync();
+            return new GetLocationCountQueryResult
+            {
+                LocationCount = values
+            };
+        }
+    }
+}
