1f4e10039dcc0ecdb3bb29f6225a7766540717d9
Haçik Ulu <hacikd2@gmail.com>
2024-05-12 18:46:10 +0300

    Filter Detail is edited.

--- Frontends/RentACarProject.WebUI/Controllers/DefaultController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/DefaultController.cs
@@ -13,6 +13,7 @@ namespace RentACarProject.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
@@ -29,6 +30,17 @@ namespace RentACarProject.WebUI.Controllers
             ViewBag.v = values2;
             return View();
 
+        }
+
+        [HttpPost]
+        public IActionResult Index(string book_pick_date, string book_off_date, string time_pick, string time_off, string locationID)
+        {
+            TempData["bookpickdate"] = book_pick_date;
+            TempData["bookoffdate"] = book_off_date;
+            TempData["timepick"] = time_pick;
+            TempData["timeoff"] = time_off;
+            TempData["locationID"] = locationID;
+            return RedirectToAction("Index", "RentCarList");
         }
     }
 }
