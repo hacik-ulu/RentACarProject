9075194bb13e2053e5b5a551dbfad41e4f743c92
Haçik Ulu <hacikd2@gmail.com>
2024-05-12 18:15:34 +0300

    Dropdown style edited.

--- Frontends/RentACarProject.WebUI/Controllers/DefaultController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using RentACarProject.Dto.LocationDtos;
 
 namespace RentACarProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Locations");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value = x.LocationID.ToString()
+                                            }).ToList();
+            ViewBag.v = values2;
             return View();
+
         }
     }
 }
