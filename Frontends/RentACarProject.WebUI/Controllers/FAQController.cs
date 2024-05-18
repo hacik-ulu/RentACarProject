6373b1834022b53952a1303a2f7e89e6665bbca8
Haçik Ulu <hacikd2@gmail.com>
2024-05-18 23:49:58 +0300

    Popup's will turn to Static Controller.

---
+++ Frontends/RentACarProject.WebUI/Controllers/FAQController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.Controllers
+{
+    public class FAQController : Controller
+    {
+        private List<string> FAQs = new List<string>
+        {
+            "Soru 1: Cevap 1",
+            "Soru 2: Cevap 2",
+            "Soru 3: Cevap 3"
+        };
+
+        // FAQ View'ı döndüren action
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.v1 = "FAQ ";
+            // FAQs listesini View'a gönder
+            return View(FAQs);
+        }
+    }
+}
--- Frontends/RentACarProject.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/RentACarProject.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.FooterAddressDtos;
+using RentACarProject.Dto.TestimonialDtos;
 
 namespace RentACarProject.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            // GetAsync used for list or get the datas.
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // We are reading data from uotcome of our Api as string format.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
