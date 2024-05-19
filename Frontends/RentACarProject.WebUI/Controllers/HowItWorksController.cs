5b16e927e664590b82e7bb10ef9c7ea33ac8138a
Haçik Ulu <hacikd2@gmail.com>
2024-05-19 23:16:48 +0300

    Footer edited.

---
+++ Frontends/RentACarProject.WebUI/Controllers/HowItWorksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.Controllers
+{
+    public class HowItWorksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var howItWorks = new List<string>
+            {
+                "1. Rent Your Car",
+                "Create an application for the vehicle you want to rent by following the instructions on our Website.",
+
+                 "2.Information Controll Process",
+                 "Our authorized units will examine your application meticulously and carefully. In this step, the accuracy of the information and the demands of our valued customers will be evaluated.",
+
+                 "3.Check Your Email",
+                 "After the evaluation phase is completed, the results will be announced via e-mail by the rental department. After completing your application, you are kindly requested to check your e-mail frequently. Your application will be responded to positively or negatively. We will state our reasons for negative feedback.",
+
+                 "4.Come to Our Office",
+                 "If the applications are positive, we will host you in our offices. Important information such as approval of the rental procedure, receipt of fees and completion of signature procedures will be provided in our offices. You are kindly requested to come to our offices in order to deliver the vehicles and obtain accurate information."
+
+            };
+
+
+            ViewBag.v1 = "HOW IT WORKS ? ";
+            ViewBag.v2 = "Rapid Rent Tips";
+
+            return View(howItWorks);
+        }
+    }
+}
---
+++ Frontends/RentACarProject.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarProject.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "PAYMENT ";
+            ViewBag.v2 = "Payment Option";
+
+            var paymentOption = new List<string>
+            {
+                "Physical Office Payments",
+                "You can easily make your rental payments by coming to our offices in our relevant centers.",
+
+                 "IBAN and Wire Transfer Payments",
+                 "By contacting our authorized units, IBAN and money transfer information will be shared with you in parallel with your e-mail information regarding the rental, if the information matches.",
+
+                 "Payment with Reference Number",
+                 "You can have the chance to win a free ride if you send your reference number via e-mail or by meeting our authorized units face to face. Important notice: The reference number is not valid for every car, you will have free rental access to suitable vehicles as a result of the e-mail you receive or as directed by our officials."
+
+            };
+
+            return View(paymentOption);
+        }
+    }
+}
