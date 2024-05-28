13f46f2a5d809f92df7f6b4d6d105661572aa6c3
Haçik Ulu <hacikd2@gmail.com>
2024-05-28 21:35:12 +0300

    Dashboards editing.

--- Frontends/RentACarProject.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChartThreeComponentPartial.cs
+++ Frontends/RentACarProject.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChartThreeComponentPartial.cs
@@ -1,6 +1,32 @@
-﻿namespace RentACarProject.WebUI.ViewComponents.DashboardComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.LocationDtos;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RentACarProject.WebUI.ViewComponents.DashboardComponents
 {
-    public class _AdminDashboardChartThreeComponentPartial
+    public class _AdminDashboardChartThreeComponentPartial : ViewComponent
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardChartThreeComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/Locations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultLocationDto>());
+        }
     }
 }
