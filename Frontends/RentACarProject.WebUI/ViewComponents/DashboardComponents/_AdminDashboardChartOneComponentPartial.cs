497a892207da576fd8e96b98f66e9ed21d32801a
Haçik Ulu <hacikd2@gmail.com>
2024-05-28 23:01:35 +0300

    AdminDashboard edited.

--- Frontends/RentACarProject.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChartOneComponentPartial.cs
+++ Frontends/RentACarProject.WebUI/ViewComponents/DashboardComponents/_AdminDashboardChartOneComponentPartial.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.CarPricingDtos;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace RentACarProject.WebUI.ViewComponents.DashboardComponents
 {
-    public class _AdminDashboardChartOneComponentPartial :ViewComponent
+    public class _AdminDashboardChartOneComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardChartOneComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7262/api/CarPricings");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
+                return View(values); 
+            }
+            return View(new List<ResultCarPricingWithCarDto>());
         }
     }
 }
