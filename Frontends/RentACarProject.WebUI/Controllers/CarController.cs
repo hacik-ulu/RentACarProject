dcff58e81ec95e0ab072768d44a8c544d0ba74e8
Haçik Ulu <hacikd2@gmail.com>
2024-05-29 13:47:25 +0300

    Car Detail editing.

--- Frontends/RentACarProject.WebUI/Controllers/CarController.cs
+++ Frontends/RentACarProject.WebUI/Controllers/CarController.cs
@@ -4,29 +4,37 @@ using RentACarProject.Dto.CarPricingDtos;
 
 namespace RentACarProject.WebUI.Controllers
 {
-    public class CarController : Controller
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public CarController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-        public async Task<IActionResult> Index()
-        {
-            ViewBag.v1 = "CARS ";
-            ViewBag.v2 = "Choose Your Car";
+	public class CarController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public CarController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IActionResult> Index()
+		{
+			ViewBag.v1 = "CARS ";
+			ViewBag.v2 = "Choose Your Car";
 
-            var client = _httpClientFactory.CreateClient();
-            // GetAsync used for list or get the datas.
-            var responseMessage = await client.GetAsync("https://localhost:7262/api/CarPricings");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                // We are reading data from uotcome of our Api as string format.
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
-    }
+			var client = _httpClientFactory.CreateClient();
+			// GetAsync used for list or get the datas.
+			var responseMessage = await client.GetAsync("https://localhost:7262/api/CarPricings");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				// We are reading data from uotcome of our Api as string format.
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> CarDetail(int id)
+		{
+			ViewBag.v1 = "CAR DETAİLS ";
+			ViewBag.v2 = "Car Specifications";
+			ViewBag.CarID = id;
+			return View();
+		}
+	}
 }
---
+++ Frontends/RentACarProject.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailMainCarFeatureComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarProject.Dto.CarDtos;
+using RentACarProject.Dto.CategoryDtos;
+using System.Net.Http;
+
+namespace RentACarProject.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailMainCarFeatureComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailMainCarFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			ViewBag.CarID = id;
+			var client = _httpClientFactory.CreateClient();
+			var resposenMessage = await client.GetAsync($"https://localhost:7262/api/Cars/{id}");
+			if (resposenMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await resposenMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<ResultCarWithBrandsDto>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
