ed78a3ea3a26a1619b9d96bd512df9aaf6b46d30
Haçik Ulu <hacikd2@gmail.com>
2024-06-01 19:27:26 +0300

    Register Mediator and Enum added.

---
+++ Core/RentACarProject.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using RentACarProject.Application.Enums;
+using RentACarProject.Application.Features.Mediator.Commands.RegisterCommands;
+using RentACarProject.Application.Interfaces.GeneralInterfaces;
+using RentACarProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUser> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new AppUser
+            {
+                Password = request.Password,
+                Username = request.Username,
+                AppRoleID = (int)RolesType.Member
+            });
+        }
+    }
+}
---
+++ Presentation/RentACarProject.WebApi/Controllers/RegistersController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACarProject.Application.Features.Mediator.Commands.RegisterCommands;
+
+namespace RentACarProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RegistersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateAppUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("User added sucessfully!");
+        }
+    }
+}
