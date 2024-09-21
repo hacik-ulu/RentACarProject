2d8af9e932c95c6dc26a3bb597343967dfcbaa8e
Haçik Ulu <hacikd2@gmail.com>
2024-09-21 16:14:11 +0300

    AdminReservation pagination added.

--- Frontends/RentACarProject.WebUI/Areas/Admin/Controllers/AdminReservationController.cs
+++ Frontends/RentACarProject.WebUI/Areas/Admin/Controllers/AdminReservationController.cs
@@ -19,13 +19,14 @@ namespace RentACarProject.WebUI.Controllers
         }
 
         [Route("Index")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
             var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
             if (token == null)
             {
                 return RedirectToAction("Index", "Login");
             }
+
             if (token != null)
             {
                 var claims = User.Claims;
@@ -34,11 +35,23 @@ namespace RentACarProject.WebUI.Controllers
                     var client = _httpClientFactory.CreateClient();
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                     var responseMessage = await client.GetAsync("https://localhost:7262/api/Reservations/GetReservationList");
+
                     if (responseMessage.IsSuccessStatusCode)
                     {
                         var jsonData = await responseMessage.Content.ReadAsStringAsync();
                         var values = JsonConvert.DeserializeObject<List<ResultReservationDto>>(jsonData);
-                        return View(values);
+
+                        // Pagination settings
+                        int pageSize = 5; // Her sayfada gösterilecek kayıt sayısı
+                        int totalRecords = values.Count; // Toplam kayıt sayısı
+                        int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize); // Toplam sayfa sayısı
+
+                        var paginatedItems = values.Skip((page - 1) * pageSize).Take(pageSize).ToList(); // Geçerli sayfaya ait verileri al
+
+                        ViewBag.CurrentPage = page;
+                        ViewBag.TotalPages = totalPages;
+
+                        return View(paginatedItems);
                     }
                 }
                 else if (claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Member"))
@@ -48,5 +61,6 @@ namespace RentACarProject.WebUI.Controllers
             }
             return View();
         }
+
     }
 }
