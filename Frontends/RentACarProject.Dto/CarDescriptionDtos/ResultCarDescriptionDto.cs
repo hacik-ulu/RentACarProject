23becc1a50ef9aa1975167652849ffbe6e05ce29
Haçik Ulu <hacikd2@gmail.com>
2024-10-08 15:27:16 +0300

    Admin panel is editing.

---
+++ Frontends/RentACarProject.Dto/CarDescriptionDtos/ResultCarDescriptionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarProject.Dto.CarDescriptionDtos
+{
+    public class ResultCarDescriptionDto
+    {
+        public int CarDescriptionID { get; set; }
+        public int CarID { get; set; }
+        public List<string> CarDetails { get; set; }
+
+        // Tek bir string olarak tutacak özellik.
+        public string CarDetailsAsString => string.Join(", ", CarDetails);
+    }
+}
--- Frontends/RentACarProject.WebUI/Areas/Admin/Controllers/AdminCarDescriptionController.cs
+++ Frontends/RentACarProject.WebUI/Areas/Admin/Controllers/AdminCarDescriptionController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Newtonsoft.Json;
 using RentACarProject.Dto.BannerDtos;
+using RentACarProject.Dto.CarDescriptionDtos;
+using RentACarProject.Dto.CarDtos;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 
@@ -17,37 +20,78 @@ namespace RentACarProject.WebUI.Areas.Admin.Controllers
         }
 
         [Route("Index")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
             var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
             if (token == null)
             {
                 return RedirectToAction("Index", "Login");
             }
-            if (token != null)
+
+            var claims = User.Claims;
+            if (claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Admin"))
             {
-                var claims = User.Claims;
-                if (claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Admin"))
+                var client = _httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var carDescriptions = new List<ResultCarDescriptionDto>();
+
+                using (var connection = new SqlConnection("Server=HACIKULU\\SQLEXPRESS;initial Catalog=RentACarDb;integrated security=true;Encrypt=True;TrustServerCertificate=True;"))
                 {
-                    // Admin ise işlemleri yap ve AdminLocation/Index sayfasına yönlendir
-                    var client = _httpClientFactory.CreateClient();
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    //https://localhost:7262/api/CarDescriptions?id=55
-                    var responseMessage = await client.GetAsync("https://localhost:7262/api/CarDescriptions?");
-                    if (responseMessage.IsSuccessStatusCode)
+                    await connection.OpenAsync();
+
+                    string query = @"
+                SELECT 
+                    cd.CarDescriptionID,
+                    c.CarID,
+                    CONCAT(b.Name, ' ', c.Model, ' (', c.Year, ')') AS Details
+                FROM 
+                    CarDescriptions cd
+                JOIN 
+                    Cars c ON cd.CarID = c.CarID
+                JOIN 
+                    Brands b ON c.BrandID = b.BrandID";
+
+                    using (var command = new SqlCommand(query, connection))
                     {
-                        var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                        var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
-                        return View(values);
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                var carDescription = new ResultCarDescriptionDto
+                                {
+                                    CarDescriptionID = reader.GetInt32(0),
+                                    CarID = reader.GetInt32(1),
+                                    CarDetails = new List<string> { reader.GetString(2) } 
+                                };
+                                carDescriptions.Add(carDescription);
+                            }
+                        }
                     }
                 }
-                else if (claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Member"))
-                {
-                    return RedirectToAction("Index", "Default");
-                }
+
+                int pageSize = 5;
+                int totalRecords = carDescriptions.Count;
+                int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+                var paginatedItems = carDescriptions.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+                ViewBag.CurrentPage = page;
+                ViewBag.TotalPages = totalPages;
+
+                return View(paginatedItems);
+            }
+            else if (claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Member"))
+            {
+                return RedirectToAction("Index", "Default");
             }
             return View();
         }
 
+
+
     }
 }
+
+
+// token olayını metod yapailiriz.
